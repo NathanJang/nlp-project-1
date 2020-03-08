04e6945d84e19bcb9b1bc79a3fb9cf7883e3acf1
Yousef Issa <yousef@u.northwestern.edu>
2020-03-07 19:55:29 -0600

    more attempts at a fix

--- gg_api.py
+++ gg_api.py
@@ -8,7 +8,6 @@ from collections import Counter
 import re
 import sys
 
-
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
@@ -44,6 +43,7 @@ def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
+    print('Entered get_hosts() function')
     global RESULTS, YEARLY_TWEETS
     YEARLY_TWEETS = get_tweets_from_file(year)
     host_tweets = tweet_handler.get_host_tweets(YEARLY_TWEETS)
@@ -56,6 +56,7 @@ def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     global AWARD_MAPPING, CURRENT_YEAR_OFFICIAL_AWARDS, RESULTS
+    print('Entered get_awards() function')
     if year is '2013' or year is '2015':
         CURRENT_YEAR_OFFICIAL_AWARDS = OFFICIAL_AWARDS_1315
     else:
@@ -72,14 +73,15 @@ def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change
     the name of this function or what it returns.'''
+    print('Entered get_nominee() function')
     global RESULTS
     nominees = {}
     stopwords = ['winner', 'this year', 'could win', 'tonight', 'next year\'s', 'next year', 'http', '@', 'rt', 'tweet', 'twitter', 'goldenglobes']
     tweet_handler = helpers.TweetHandler()
-    # nominee_tweets = tweet_handler.get_nominee_tweets(YEARLY_TWEETS)
+    nominee_tweets = tweet_handler.get_nominee_tweets(YEARLY_TWEETS)
     for award in CURRENT_YEAR_OFFICIAL_AWARDS:
         relevant_tweets = [
-            tweet for tweet in YEARLY_TWEETS
+            tweet for tweet in nominee_tweets
             if award.lower()[0:len(award) // 2] in tweet.lower()
         ]
         # some awards are people vs works of art
@@ -100,6 +102,12 @@ def get_nominees(year):
         c = Counter(potential_nominees)
 
         nominees[award] = list(c.keys())
+
+    # cleanup to fix the presenter crash, add missing awards in
+    for award_name in CURRENT_YEAR_OFFICIAL_AWARDS:
+        if award_name not in nominees:
+            nominees[award_name] = []
+
     RESULTS['nominees'] = nominees
     return nominees
 
@@ -109,20 +117,27 @@ def get_winner(year):
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
     global RESULTS
+    print('Entered get_winner() function')
     winners = {}
     for award in CURRENT_YEAR_OFFICIAL_AWARDS:
         try:
             winners[award] = RESULTS['nominees'][award][0]
         except IndexError:
             pass
+    # cleanup to fix the presenter crash, add missing awards in
+    for award_name in CURRENT_YEAR_OFFICIAL_AWARDS:
+        if award_name not in winners:
+            winners[award_name] = []
     RESULTS['winners'] = winners
     return winners
+
 
 def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
     global RESULTS
+    print('Entered get_presenters() function')
     pattern = 'present[^a][\w]*\s([\w]+\s){1,5}'
     presenter_pattern = re.compile(pattern)
     found_presenters = {}
@@ -144,12 +159,17 @@ def get_presenters(year):
                 if text_match:
                     matched = tweet[0:text_match.span()[1]]
                     awards_tweets.append(matched)
-            potential_presenters = tweet_tokenizer.get_presenters(awards_tweets, award, RESULTS['nominees'])
-            presenter_counter = Counter(potential_presenters)
-            if len(presenter_counter.most_common(1)):
-                found_presenters[award] = [presenter[0] for presenter in presenter_counter.most_common(2) if presenter]
+        potential_presenters = tweet_tokenizer.get_presenters_tokens(awards_tweets, award, RESULTS['nominees'])
+        presenter_counter = Counter(potential_presenters)
+        if len(presenter_counter.most_common(1)):
+            found_presenters[award] = [presenter[0] for presenter in presenter_counter.most_common(2) if presenter]
+    # cleanup to fix the presenter crash, add missing awards in
+    for award_name in CURRENT_YEAR_OFFICIAL_AWARDS:
+        if award_name not in found_presenters:
+            found_presenters[award_name] = []
     RESULTS['presenters'] = found_presenters
     return found_presenters
+
 
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
@@ -159,6 +179,7 @@ def pre_ceremony():
     # Your code here
     print("Pre-ceremony processing complete.")
     return
+
 
 def main():
     '''This function calls your program. Typing "python gg_api.py"
@@ -180,8 +201,8 @@ def main():
     get_awards(year)
     get_nominees(year)
     get_winner(year)
-    # get_presenters(year)
-    RESULTS['presenters'] = {award: [] for award in CURRENT_YEAR_OFFICIAL_AWARDS}
+    get_presenters(year)
+    # RESULTS['presenters'] = {award: [] for award in CURRENT_YEAR_OFFICIAL_AWARDS}
     our_results = output_handler.print_results(RESULTS, CURRENT_YEAR_OFFICIAL_AWARDS)
     print(our_results)
     return
